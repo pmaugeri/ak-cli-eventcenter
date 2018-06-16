0d8d82c5db754e9d54bb3e716bc350302300bd18
pmaugeri <pmaugeri2@gmail.com>
2018-06-16 08:38:49 +0000

    Hello World

---
+++ src/eventcenter.js
@@ -0,0 +1,12 @@
+'use strict';
+
+class akEventCenter {
+
+    makeRequest(request) {
+        return new Promise ((resolve, reject) => {
+		console.log("Hello World!")
+        })
+    }
+
+}
+module.exports = akEventCenter;
